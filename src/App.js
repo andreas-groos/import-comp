e5b5cd84de81ea32649c1948f410cf3a3eab3022
Andreas Groos <andreas.groos1@gmail.com>
2020-11-16 12:53:47 -0800

    added .npmrc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Test } from '@andreas-groos/export-comp'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* <Test message="Hi there" /> */}
       </header>
     </div>
   );
